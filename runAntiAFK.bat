421012adccedf54f4970c1eb18ecb3e466cc7b03
WillGeis <willgeister@gmail.com>
2025-07-11 15:06:04 -0500

    added a shell executable for mac users

--- runAntiAFK.bat
+++ runAntiAFK.bat
@@ -1,10 +1,7 @@
 @echo off
 echo Starting Mouse Jiggler...
 
-:: Change to the directory where your Python script is located
-cd /d "C:\Path\To\Your\Script"
-
 :: Run the script (make sure Python is added to your PATH)
-python mouse_jiggler.py
+python AntiAFK.py
 
 pause
---
+++ runAntiAFK.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Starting Mouse Jiggler..."
+
+# Run the Python script (assumes Python is in your PATH)
+python3 AntiAFK.py
+
+# Pause equivalent: waits for user to press Enter
+read -p "Press Enter to continue..."
