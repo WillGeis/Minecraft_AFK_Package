1c9e2a0bb7da9600b1bc3ccf5b1285afe28d27e3
WillGeis <willgeister@gmail.com>
2025-09-25 11:52:21 -0500

    correctly handles time intervals

--- AntiAFK_click.py
+++ AntiAFK_click.py
@@ -5,27 +5,27 @@ import time
 def click():
     print("Press 'R' for random time, press 'T' for fixed time.")
     choice = input("Enter your choice: ")
-    if choice.casefold() == 'R'.casefold():
+    if choice.casefold() == 'r':
         while True:
-            time = random.randint(1, 100)
+            wait_time = random.randint(1, 100)
             pyautogui.click()
-            print(f"Mouse clicked {time} seconds")
-            time.sleep(10 + time)
-    elif choice.casefold() == 'T'.casefold():
+            print(f"Mouse clicked {wait_time} seconds")
+            time.sleep(10 + wait_time)
+    elif choice.casefold() == 't':
         clickfixed()
-        
 
 def clickfixed():
     print("Enter the fixed time interval in seconds:")
     choice2 = input()
-    if input.isdigit() == False:
+    if not choice2.isdigit():
         print("Please enter a valid number.")
         clickfixed()
+        return
+    wait_time = int(choice2)
     while True:
-        time = choice2
         pyautogui.click()
         print("Mouse clicked.")
-        time.sleep(10 + time)
+        time.sleep(10 + wait_time)
 
 if __name__ == "__main__":
     click()
